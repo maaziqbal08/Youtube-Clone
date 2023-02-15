69b4793bf45fec774146c9f71122399f7aa78390
maaziqbal08 <maaziqbal631@gmail.com>
2023-02-15 15:19:03 +0500

    Youtube Clone

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "3230a7ae0bmshc5e06d1c838b2fbp18b566jsn85e483f3e689",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
